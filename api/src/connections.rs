02b93ca0e20c30a69977cf7f7da0818a32277b4d
Alex <climie.alex@gmail.com>
2025-02-11 11:40:31 +0800

    forgot to add new files

---
+++ api/src/connections.rs
@@ -0,0 +1,12 @@
+// a connection between two people is described as an integer
+// between 0 - 10
+
+// the map of all connections is the connections between all people
+
+pub async fn get_connections() -> Vec<(((String, u64), (String, u64)), u64)> {
+    vec![]
+}
+
+pub async fn update_connection(id1: u64, id2: u64, delta: u64) {
+    
+}
